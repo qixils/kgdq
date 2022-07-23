23023ed07e3e0e6462db2260cf1cf2b949a595fb
Lexi <lexi@qixils.dev>
2022-07-23 14:08:25 -0400

    add missing run ModelType

--- kgdq/src/main/kotlin/dev/qixils/gdq/ModelType.kt
+++ kgdq/src/main/kotlin/dev/qixils/gdq/ModelType.kt
@@ -2,11 +2,16 @@ package dev.qixils.gdq
 
 import dev.qixils.gdq.models.Event
 import dev.qixils.gdq.models.Model
+import dev.qixils.gdq.models.Run
 import dev.qixils.gdq.models.Runner
 import kotlinx.serialization.KSerializer
 import kotlin.reflect.KClass
 
-data class ModelType<M : Model>(val id: String, val kClass: KClass<M>, val serializer: KSerializer<M>) {
+data class ModelType<M : Model>(
+    val id: String,
+    val kClass: KClass<M>,
+    val serializer: KSerializer<M>)
+{
     val jClass: Class<M> = kClass.java
 
     companion object {
@@ -18,7 +23,8 @@ data class ModelType<M : Model>(val id: String, val kClass: KClass<M>, val seria
 
         fun get(id: String): ModelType<*>? = types[id.replaceFirst("tracker.", "")]
 
-        val EVENT: ModelType<Event> = register(ModelType("event", Event::class, Event.serializer()))
-        val RUNNER: ModelType<Runner> = register(ModelType("runner", Runner::class, Runner.serializer()))
+        val EVENT = register(ModelType("event", Event::class, Event.serializer()))
+        val RUN = register(ModelType("run", Run::class, Run.serializer()))
+        val RUNNER = register(ModelType("runner", Runner::class, Runner.serializer()))
     }
 }
