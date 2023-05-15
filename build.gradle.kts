139263af423034042f3aba81e26ad0ff735018af
Lexi Larkin <lexi@qixils.dev>
2023-05-15 18:17:26 -0400

    feat: dependabot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 val kotlin_version: String by project
 
+// TODO: move dependency versions to gradle/libs.versions.toml for dependabot
+
 plugins {
     kotlin("jvm") version "1.7.10" apply true
     kotlin("plugin.serialization") version "1.7.10" apply false
