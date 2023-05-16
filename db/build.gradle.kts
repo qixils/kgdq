9dd92a66b06f1797c1998926a0c374ffbdeaded3
Lexi Larkin <lexi@qixils.dev>
2023-05-15 21:10:40 -0400

    build(deps): bump slf4j from 2.0.1 to 2.0.7

--- db/build.gradle.kts
+++ db/build.gradle.kts
@@ -1,10 +1,11 @@
 val serialization_version: String by project
+val slf4j_version: String by project
 
 plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.21"
 }
 
 dependencies {
-    implementation("org.slf4j:slf4j-api:2.0.6")
+    implementation("org.slf4j:slf4j-api:$slf4j_version")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor:$serialization_version")
 }
