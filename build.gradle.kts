1d9a1b675f979f97f3ec6ef4b192096df37c8082
Lexi Larkin <lexi@qixils.dev>
2023-05-24 13:27:53 -0400

    fix: use jvm toolchain
    fixes warning about mismatched JRE targets in java/kotlin tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,20 +13,17 @@ allprojects {
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
 
-    val javaVer = JavaVersion.VERSION_17
-    java.sourceCompatibility = javaVer
-    java.targetCompatibility = javaVer
-
     dependencies {
         api("org.jetbrains.kotlin:kotlin-stdlib:1.8.21")
     }
 
     tasks.compileKotlin {
-        kotlinOptions {
-            jvmTarget = javaVer.majorVersion
-        }
         kotlinOptions.freeCompilerArgs += "-opt-in=dev.qixils.gdq.InternalGdqApi"
         kotlinOptions.freeCompilerArgs += "-opt-in=dev.qixils.horaro.InternalHoraroApi"
+    }
+
+    kotlin {
+        jvmToolchain(17)
     }
 }
 
