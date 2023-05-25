9b2276dca1b53b22a801188534adb095d7cf053b
Lexi Larkin <lexi@qixils.dev>
2023-05-25 14:11:19 -0400

    fix: event page

--- web/src/routes/event/[org]/+page.ts
+++ web/src/routes/event/[org]/+page.ts
@@ -1,7 +1,8 @@
 import {SvcClient} from "vods.speedrun.club-client";
+import type {PageLoad} from './$types';
 
 const svc = new SvcClient();
 
-export async function load(params: {org: string}) {
+export const load = (async ({ params }) => {
     return { org: await svc.getMarathon(params.org, false) };
-}
+}) satisfies PageLoad;
