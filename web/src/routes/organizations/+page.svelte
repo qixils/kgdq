b977341f6a1ac11b880cfd427a5156bd6ade7083
Lexi Larkin <lexi@qixils.dev>
2023-05-25 20:41:35 -0400

    feat: org descriptions

--- web/src/routes/organizations/+page.svelte
+++ web/src/routes/organizations/+page.svelte
@@ -16,17 +16,27 @@
 {#each data.orgs as org (org.id)}
     <section>
         <h2>{org.displayName}</h2>
-        {#if org.id === "gdq"}
-            <p>Commonly known as GDQ, very popular and hosts <abbr title="Awesome Games Done Quick">AGDQ</abbr> and <abbr title="Summer Games Done Quick">SGDQ</abbr> every winter and summer in the US.</p>
+        <p>{#if org.id === "gdq"}
+            Also known as GDQ, this highly popular American organization hosts two main week-long events each year,
+            <abbr title="Awesome Games Done Quick">AGDQ</abbr> and <abbr title="Summer Games Done Quick">SGDQ</abbr>.
+            The former is held in the winter for the benefit of the
+            <a href="https://preventcancer.org/">Prevent Cancer Foundation</a> while the latter is held in the summer
+            for the benefit of <a href="https://doctorswithoutborders.org/">Doctors Without Borders</a>.
+            GDQ also hosts two Frame Fatales events made by and for women and non-binary speedrunners each year for the
+            benefit of the <a href="https://malala.org/">Malala Fund</a>.
         {:else if org.id === "esa"}
-            <p>ESA also hosts two annual events, ESA Winter and ESA Summer, in Sweden.</p>
+            ESA is the largest European speedrunning marathon and holds two events in Sweden each year. ESA Winter
+            benefits <a href="https://savethechildren.org/">Save the Children</a> while ESA Summer benefits
+            <a href="https://www.alzheimersgameover.com/">Alzheimerfonden</a>.
         {:else if org.id === "hek"}
-            <p>TODO</p>
+            A spin-off of ESA started by the titular Hekigan, this incredibly silly marathon raises money for the same
+            charities as ESA each year.
         {:else if org.id === "rpglb"}
-            <p>TODO</p>
+            RPGLB holds an annual event all about RPGs for the benefit of the
+            <a href="https://www.nami.org/">National Institute on Mental Illness</a>.
         {:else}
-            <p>We don't know much about this organization yet but we're sure they're lovely and you should support them.</p>
-        {/if}
+            We don't know much about this organization yet but we're sure they're lovely and you should support them.
+        {/if}</p>
         <ul>
             <li><a href={org.homepageUrl} target="_blank">{org.shortName} Homepage</a></li>
             <li><a href="/event/{org.id}">View {org.shortName}-hosted events</a></li>
