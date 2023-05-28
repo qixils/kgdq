65456c37780e752eda8ae7e956240d06777eab3a
Dunkyl <38872099+dunkyl@users.noreply.github.com>
2023-05-27 23:40:53 -0700

    set user store

--- web/src/routes/logged-in/+page.svelte
+++ web/src/routes/logged-in/+page.svelte
@@ -3,26 +3,25 @@
     import {onMount} from "svelte";
     import type {User} from "$lib/kgdq.ts";
     import {BASE_URL, getUser} from "$lib/kgdq.ts";
+    import {user} from "../../stores";
 
-    let user: User | null = null;
+    // let user: User | null = null;
     let user_error: string | null = null;
 
 
 
     onMount(async () => {
         try {
-            user = await getUser();
+            $user = await getUser();
         } catch (e) {
             user_error = e;
             console.error(e);
         }
-
-        localStorage.setItem("user", JSON.stringify(user));
     });
 </script>
 
-{#if user}
-    <p>Logged in as {user.name}</p>
+{#if $user}
+    <p>Logged in as {$user.name}</p>
 {:else if user_error}
     <p>Error loading user: {user_error}</p>
 {:else}
