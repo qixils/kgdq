6d8a1260221cce96a8c4a8ca43c8cafb7c5da9a9
Lexi Larkin <lexi@qixils.dev>
2023-07-10 20:53:39 -0400

    build(deps): bump to gradle 8, kotlin 1.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.8.22" apply true
-    kotlin("plugin.serialization") version "1.8.22" apply false
-    id("com.github.johnrengelman.shadow") version "7.1.2" apply false
+    kotlin("jvm") version "1.9.0" apply true
+    kotlin("plugin.serialization") version "1.9.0" apply false
+    id("com.github.johnrengelman.shadow") version "8.1.1" apply false
 }
 
 allprojects {
