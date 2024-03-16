605f83e2ea53dfa4b3297450b558e8e6ac25e6f0
Dunkyl <38872099+dunkyl@users.noreply.github.com>
2024-03-15 23:06:27 -0700

    dark mode button bg

--- web/src/lib/BidTarget.svelte
+++ web/src/lib/BidTarget.svelte
@@ -2,20 +2,19 @@
     import {Formatters} from "$lib/Formatters";
     import type {Bid} from "vods.speedrun.club-client";
 
-    export let bid: Bid = undefined as Bid;
+    export let bid: Bid;
     export let formatter: Formatters;
+    let goal = bid.goal || 1;
 
-    let percent = Math.round((bid.donationTotal / bid.goal) * 100);
-    let color = "--bid-" + (percent >= 100 ? "full" : (bid.state === "OPENED" ? "open" : "closed"));
-    let textColor = `var(${color}-text)`;
-    color = `var(${color})`;
+    let percent = Math.round((bid.donationTotal / (bid.goal || goal)) * 100);
+    let color = `var(--bid-${percent >= 100 ? "full" : (bid.state === "OPENED" ? "open" : "closed")})`;
 
 </script>
 
 <span class="radial-progress" style="--value:{percent}; --size:30px; color:{color}"></span>
 <div class="bid-body">
     <b>{bid.name}</b>
-    <span class="bid-contributions">{ formatter.money(bid.donationTotal) } / { formatter.money(bid.goal) }</span>
+    <span class="bid-contributions">{ formatter.money(bid.donationTotal) } / { formatter.money(goal) }</span>
 </div>
 {#if bid.description}
     <div class="bid-description">{bid.description}</div>
