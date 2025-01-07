0183a3b4d16d7b8e03ade8a360f842d3eb1f4f1e
Lexi Larkin <lexi@qixils.dev>
2025-01-06 19:18:16 -0500

    fix: milliseconds hotfix

--- serializers/src/main/kotlin/dev/qixils/gdq/serializers/DurationSerializer.kt
+++ serializers/src/main/kotlin/dev/qixils/gdq/serializers/DurationSerializer.kt
@@ -11,6 +11,7 @@ import kotlinx.serialization.encoding.Encoder
 import java.time.Duration
 import java.util.regex.Pattern
 import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.toJavaDuration
@@ -27,7 +28,7 @@ class DurationAsStringSerializer : KSerializer<Duration> {
     }
 
     companion object {
-        private val pattern: Pattern = Pattern.compile("\\d+:\\d{2}:\\d{2}")
+        private val pattern: Pattern = Pattern.compile("\\d+:\\d{2}:\\d{2}(?:\\.\\d{1,3})?")
         private const val gdqFormat: String = "%d:%02d:%02d"
 
         fun format(duration: Duration, format: String = gdqFormat): String {
@@ -39,6 +40,7 @@ class DurationAsStringSerializer : KSerializer<Duration> {
                 duration.toSecondsPart()
             )
         }
+
         fun decode(input: String): Duration {
             // fix for GDQ API returning "0" instead of "0:00:00" for 0 duration
             if (input == "0")
@@ -47,8 +49,8 @@ class DurationAsStringSerializer : KSerializer<Duration> {
             val matcher = pattern.matcher(input)
             if (!matcher.matches())
                 throw SerializationException("Invalid duration format: $input")
-            val parts = matcher.group(0).split(":")
-            return (parts[0].toInt().hours + parts[1].toInt().minutes + parts[2].toInt().seconds).toJavaDuration()
+            val parts = matcher.group(0).split(":", ".")
+            return (parts[0].toInt().hours + parts[1].toInt().minutes + parts[2].toInt().seconds + (parts.getOrElse(3) { null }?.toIntOrNull() ?: 0).milliseconds).toJavaDuration()
         }
     }
 }
