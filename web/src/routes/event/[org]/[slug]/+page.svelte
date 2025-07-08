dbbb8c24763c356b9594437a5d8b0cbe7ca8901e
Dunkyl <38872099+dunkyl@users.noreply.github.com>
2025-07-07 18:41:23 -0700

    collapsible days

--- web/src/routes/event/[org]/[slug]/+page.svelte
+++ web/src/routes/event/[org]/[slug]/+page.svelte
@@ -28,6 +28,8 @@
         hideBids = x;
     };
 
+    let runs_ui_spans: { index: number; day_progess_class: string; day_text: string; run_indices: number[]; hide: boolean }[] = [];
+
     onMount(async () => {
         hideBids = JSON.parse(localStorage.getItem("hideBids") || "false");
         try {
@@ -38,11 +40,29 @@
                     current_run_index = runs.indexOf(current_run);
                 }
 
-                const { hash } = $page.url
-                if (hash?.match(/^#run-\d+$/)) {
-                    document.querySelector(hash)?.scrollIntoView()
-                }
             }
+            let runs_ui_span_index = 0;
+            for (let run_index = 0; run_index < runs.length; ++run_index) {
+                let run = runs[run_index];
+                let last_run = runs[run_index - 1];
+                let last_run_status = last_run?.timeStatus;
+                let this_run_status = run.timeStatus;
+                if (run_index === 0 || new Date(run.startTime).getDay() !== new Date(last_run.startTime).getDay()) {
+                    //  fill line for day if the next displayed run is at least next up:
+                    let day_progess_class = (this_run_status !== "UPCOMING" || last_run_status === "FINISHED") ? "finished" : "";
+                    let day_text = Formatters.date_weekday_date(run.startTime);
+                    runs_ui_spans.push({
+                        index: runs_ui_span_index,
+                        day_progess_class,
+                        day_text,
+                        run_indices: <number[]>[],
+                        hide: false,
+                    })
+                    ++runs_ui_span_index;
+                }
+                runs_ui_spans[runs_ui_spans.length-1].run_indices.push(run_index);
+            }
+            
         } catch (e) {
             run_error = e;
         }
@@ -108,22 +128,28 @@
     {:else if runs !== undefined}
         <ul class="event-runs" class:hide-bids={hideBids}>
 
-            {#each runs as run, run_index}
-
-                { @const last_run = runs[run_index - 1] }
-                { @const last_run_status = last_run?.timeStatus }
-                { @const this_run_status = run.timeStatus }
-
-                {#if run_index === 0 || new Date(run.startTime).getDay() !== new Date(last_run.startTime).getDay()}
-
-                    <li class='event-day {
-                        (this_run_status !== "UPCOMING" || last_run_status === "FINISHED") ? "finished" : ""
-                        }'> <!-- fill line for day if the next displayed run is at least next up  -->
+            {#each runs_ui_spans as span, span_index}
+                {@const prog = span.day_progess_class }
+                {@const next_prog = runs_ui_spans[span_index+1]?.day_progess_class }
+                <li class='event-day {prog == 'finished' && next_prog == '' && span.hide ? 'in-progress' : prog}'>
                         <div class="schedule-bar-bit"></div>
-                        <h2>{ Formatters.date_weekday_date(run.startTime) }</h2>
+                    <h2>
+                        <button class="collapse-btn" on:click={ evt => {
+                            span.hide = !span.hide;
+                        } } >
+                             <span class="material-symbols-rounded" style="font-size: 20pt; color: var(--text-quiet)" >
+                                {span.hide ? 'expand_all' : 'collapse_all'}
+                            </span>
+                            { span.day_text }
+                        </button>
+                    </h2>
+                    
                     </li>
+                {#if !span.hide }
+                    {#each span.run_indices as run_index}
+                        <RunComponent {runs} {run_index} {formatter} />
+                    {/each}
                 {/if}
-                <RunComponent {runs} {run_index} {formatter} />
             {:else}
                 <p>
                     {#if data.event.timeStatus === "UPCOMING"}
