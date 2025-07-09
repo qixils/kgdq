b625be2a7c62a989eaf33f6a3d3f9253db3f6f2b
Lexi Larkin <lexi@qixils.dev>
2025-07-08 22:08:37 -0400

    fix: vod counter
    Co-authored-by: Dunkyl <38872099+dunkyl@users.noreply.github.com>

--- web/src/lib/VODs.svelte
+++ web/src/lib/VODs.svelte
@@ -3,11 +3,11 @@
     import VODEntry from "$lib/VODEntry.svelte";
 
     export let run: Run;
-    let vodCount = new Map();
 </script>
 
 <ul class="vods-list">
-    {#each run.vods as vod}
-        <VODEntry vod={vod} index={vodCount.set(vod.type, (vodCount.get(vod.type) ?? -1) + 1).get(vod.type)} />
+    {#each run.vods as vod, i}
+        {@const index = run.vods.slice(0, i).reduce((part, v) => vod.type == v.type ? part + 1 : part, 0)}
+        <VODEntry {vod} {index} />
     {/each}
 </ul>
